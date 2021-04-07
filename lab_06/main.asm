79c29f292b149144d195f4271e015eba8c839cdd
Dmitry Varin <varindv@gmail.com>
2021-04-07 18:18:51 +0300

    lab_06

--- lab_06/main.asm
+++ lab_06/main.asm
@@ -14,74 +14,55 @@ STK SEGMENT PARA STACK 'STACK'
 STK ENDS
 
 SEGDATA SEGMENT PARA PUBLIC 'DATA'
-	menu_print  DB "1. Input sign num in binary format"
-	        DB 10
-	        DB 13
-	        DB "2. Output num in binary format"
-	        DB 10
-	        DB 13
-	        DB "3. Output num in unsigned oct format"
-	        DB 10
-	        DB 13
-	        DB "4. Output num in signed hex format"
-	        DB 10
-	        DB 13
-	        DB "5. Exit"
-	        DB 10
-	        DB 13
-	        DB "Enter mode: $"
-	actions DW read_bin_sign, print_bin_sign, print_oct_unsign, print_hex, exit
+	menu_print DB "1. Input sign num in binary format"
+	           DB 10
+	           DB 13
+	           DB "2. Output num in binary format"
+	           DB 10
+	           DB 13
+	           DB "3. Output num in unsigned oct format"
+	           DB 10
+	           DB 13
+	           DB "4. Output num in signed hex format"
+	           DB 10
+	           DB 13
+	           DB "5. Exit"
+	           DB 10
+	           DB 13
+	           DB "Enter mode: $"
+	actions    DW read_bin_sign, print_bin_sign, print_oct_unsign, print_hex, exit
 SEGDATA ENDS
 
 SEGCODE SEGMENT PARA PUBLIC "CODE"
-	ASSUME CS:SEGCODE, DS:SEGDATA, SS:STK
+	        ASSUME CS:SEGCODE, DS:SEGDATA, SS:STK
     
+	main:   
+	        mov    AX, SEGDATA
+	        mov    DS, AX
+			menu:   
+				mov    ah, 9
+				mov    dx, offset menu_print
+				int    21h
+				mov    ah, 1
+				int    21h
 
-main:   
-    mov    AX, SEGDATA
-    mov    DS, AX
-    ; mov actions[0], read_bin_sign
-    ; mov actions[2], print_bin_sign
-    ; mov actions[4], print_oct_unsign
-    ; mov actions[10], exit
+				mov    ah, 0
+				sub    al, "1"
+				mov    dl, 2
+				mul    dl
+				mov    bx, ax
+				;newline13
+				mov    ah, 2
+				mov    dl, 10
+				int    21H
+				mov    dl, 13
+				int    21H
 
-    menu:
-        mov    ah, 9
-        mov    dx, offset menu_print
-        int    21h
-
-        mov     ah, 1
-        int     21h
-
-        mov ah, 0
-        sub al, "1"
-        mov dl, 2
-        mul dl
-        mov bx, ax
-
-        ;newline
-        mov ah, 2
-        mov dl, 10
-        int 21H
-        mov dl, 13
-        int 21H
-
-        call actions[bx]
-
-    jmp menu
-
-newline:
-    mov ah, 2
-    mov dl, 10
-    int 21H
-    mov dl, 13
-    int 21H
-
-exit:
-    mov ax, 4c00h
-    int 21h
-
+				call   actions[bx]
+				jmp    menu
+	exit:
+	        mov    ax, 4c00h
+	        int    21h
 SEGCODE ENDS
 END main
-
 
