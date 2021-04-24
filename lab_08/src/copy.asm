d95ca8ffb9ed46613df17321e2a0e2558128fd4d
Dmitry Varin <varindv@gmail.com>
2021-04-24 21:21:19 +0300

    add .asm module

---
+++ lab_08/src/copy.asm
@@ -0,0 +1,33 @@
+global my_strcpy ; my_strcpy(dst, src, len);
+
+section .text
+
+my_strcpy:
+    mov rcx, rdx  ;my_strcpy(rdi, rsi, rdx)
+
+    cmp rdi, rsi ; src == dst ? quit : not_equal()
+    jne not_equal
+    jmp quit
+
+not_equal: ; строки не перекрываются
+    cmp rdi, rsi ; rdi < rsi == dst string < src string
+    jl simple_copy
+
+    mov rax, rdi
+    sub rax, rsi
+
+    cmp rax, rcx
+    jge simple_copy
+
+complicated_copy: ; строки перекрываются
+    add rdi, rcx ; смещаемся на длину и копируем с конца
+    add rsi, rcx
+    dec rsi
+    dec rdi
+    std ; df = 1 rsi-- rdi--
+
+simple_copy:
+    rep movsb ; from rsi to rdi while df==0  len == rcx раз
+    cld ; df = 0
+quit:
+    ret
