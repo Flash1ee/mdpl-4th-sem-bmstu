42a7e72b6b98bf8653baad7073b483de7306f405
Dmitry Varin <varindv@gmail.com>
2021-05-14 19:39:55 +0300

    lab_10 code complete

--- lab_10/src/main.c
+++ lab_10/src/main.c
@@ -1,39 +1,11 @@
 #include <stdio.h>
 #include "scalar_prod.h"
 
+#define COUNT 1000000000
+
 int main() {
-    puts("TEST C IMPLEMENTATION");
-    printf("%zu\n", sizeof(float));
 
-    vector_t vec_a[3] = {{1, 1, 1, 1},
-                         {2, 2, 2, 2},
-                         {3, 3, 3, 3}};
-    vector_t vec_b[3] = {{1, 1, 1,1},
-                         {2, 2, 2,2},
-                         {3, 3, 3,3}};
-
-    for (size_t i = 0; i < 3; i++) {
-        double res = 0.0;
-
-        puts("INPUT VECTORS");
-        printf("vec_a: ");
-        print(&vec_a[i]);
-        printf("vec_b: ");
-        print(&vec_b[i]);
-
-        c_scalar_prod(&res, &vec_a[i], &vec_b[i]);
-        float res_sse=0.0;
-        avx_scalar_prod(&res_sse, &vec_a[i], &vec_b[i]);
-
-        puts("result: ");
-        printf("%lf \n", res);
-        printf("%lf \n", res_sse);
-
-
-
-
-    }
-
+    execution_test(COUNT);
 
     puts("Hello world!");
     return 0;
--- lab_10/src/scalar_prod.c
+++ lab_10/src/scalar_prod.c
@@ -1,16 +1,18 @@
 //
 // Created by flashie on 12.05.2021.
 //
+#include <time.h>
+#include <float.h>
 #include "assert.h"
 #include "scalar_prod.h"
 
-void c_scalar_prod(double *dst, const vector_t *src_a, const vector_t *src_b) {
+void c_scalar_prod(float *dst, const vector_t *src_a, const vector_t *src_b) {
     assert(src_a && src_b);
     *dst = src_a->buf[0] * src_b->buf[0] + src_a->buf[1] * src_b->buf[1] + src_a->buf[2] * src_b->buf[2] +
            src_a->buf[3] * src_b->buf[3];
 }
 
-void avx_scalar_prod(float *dst, const vector_t *src_a, const vector_t *src_b) {
+void sse_scalar_prod(float *dst, const vector_t *src_a, const vector_t *src_b) {
     assert(src_a && src_b);
     __asm__(".intel_syntax noprefix\n\t"
             "movaps xmm0, %1\n\t"
@@ -25,6 +27,38 @@ void avx_scalar_prod(float *dst, const vector_t *src_a, const vector_t *src_b) {
     :"=m"(*dst)
     :"m"(src_a->buf), "m"(src_b->buf)
     :"xmm0", "xmm1");
+}
+
+void execution_test(size_t count) {
+//    float a = FLT_MAX;
+//    float b = FLT_MAX;
+//    float c = FLT_MAX;
+    float a = 10;
+    float b = 20;
+    float c = 30;
+
+    float res_c = 0;
+    vector_t vec_a[3] = {a, b, c};
+    vector_t vec_b[3] = {a, b, c};
+
+    puts("TEST C IMPLEMENTATION");
+    clock_t start = clock();
+    for (size_t i = 0; i < count; i++) {
+        c_scalar_prod(&res_c, vec_a, vec_b);
+    }
+    clock_t end_c = clock() - start;
+    printf("C scalar multiply\n\t%.3g s\n", ((double) (end_c)) / CLOCKS_PER_SEC / (double) count);
+    puts("TEST SSE ASSEMBLY");
+    start = clock();
+    float res_asm = 0;
+
+    for (size_t i = 0; i < count; i++) {
+        sse_scalar_prod(&res_asm, vec_a, vec_b);
+    }
+    clock_t end_asm = clock() - start;
+    printf("SSE ASM scalar multiply\n\t%.3g s\n", ((double) (end_asm)) / CLOCKS_PER_SEC / (double) count);
+    printf("LAST VALUES ARE EQUALS %d\n", res_asm==res_c);
+    printf("C/ASM = %lf\n", (double)end_c/(double)end_asm);
 }
 
 void print(vector_t *vec) {
